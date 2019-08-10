6117294293786153ab8f7a1977f49864143dc974
Imran Hashmi <imranhashmi27@gmail.com>
2019-08-10 17:15:36 +0100

    Added plurasight asp dotnet core fundamentals - drilling in to data

---
+++ plurasight/aspdotnetcore/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scotts Pizza", Location = "Maryland", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Bills Pizza", Location = "London", Cuisine = CuisineType.Indian},
+                new Restaurant { Id = 3, Name = "Javiers Pizza", Location = "Barcelona", Cuisine = CuisineType.Mexican}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
